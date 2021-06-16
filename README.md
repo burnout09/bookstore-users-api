ab1c0ebcaef6be15f5e37b6b5f7cb54feea3723d
Pool Malca <pmmalca@indracompany.com>
2021-06-16 15:40:46 -0500

    11. Cómo insertar filas

--- README.md
+++ README.md
@@ -5,3 +5,19 @@
 
 ## Variables de Entorno
 - mysql_users_username=root;mysql_users_password=burnout;mysql_users_host=127.0.0.1:3306;mysql_users_schema=users_db
+
+## Creacion de Table
+
+````
+create table users
+(
+id           bigint auto_increment
+primary key,
+first_name   varchar(45) null,
+last_name    varchar(45) null,
+email        varchar(45) not null,
+date_created varchar(45) null,
+constraint users_email_uindex
+unique (email)
+);
+````
--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -5,6 +5,12 @@ import (
 	"github.com/burnout09/bookstore-users-api/datasources/mysql/users_db"
 	"github.com/burnout09/bookstore-users-api/utils/date_utils"
 	"github.com/burnout09/bookstore-users-api/utils/errors"
+	"strings"
+)
+
+const (
+	indexUniqueEmail = "users_email_uindex"
+	queryInsertUser  = "INSERT INTO users(first_name,last_name,email,date_created) VALUES(?,?,?,?);"
 )
 
 var (
@@ -30,17 +36,40 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersBD[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
 	user.DateCreated = date_utils.GetNowString()
 
-	usersBD[user.Id] = user
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user :%s", err.Error()))
+	}
+
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user :%s", err.Error()))
+	}
+
+	user.Id = userId
+
+	//current := usersBD[user.Id]
+	//if current != nil {
+	//	if current.Email == user.Email {
+	//		return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
+	//	}
+	//	return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	//}
+	//
+
+	//
+	//usersBD[user.Id] = user
 
 	return nil
 }
