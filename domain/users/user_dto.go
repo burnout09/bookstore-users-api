fb0d92a94ac8224144c07bb5b0b27a6712e35fe4
Pool Malca <pmmalca@indracompany.com>
2021-06-16 11:27:28 -0500

    9. Trabajar con fechas

--- domain/users/user_dto.go
+++ domain/users/user_dto.go
@@ -8,7 +8,7 @@ import (
 type User struct {
 	Id          int64  `json:"id"`
 	FirstName   string `json:"first_name"`
-	LastName    string `json:"last_name,omitempty"`
+	LastName    string `json:"last_name"`
 	Email       string `json:"email"`
 	DateCreated string `json:"date_created"`
 }
