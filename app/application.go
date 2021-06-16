bf84e731ddd4b5c1e69f23e66427592e4fd17ad2
Pool Malca <pmmalca@indracompany.com>
2021-06-15 21:35:21 -0500

    6. Estructura básica de la aplicación: patrón MVC

--- app/application.go
+++ app/application.go
@@ -8,5 +8,8 @@ var (
 
 func StartApplication() {
 	MapUrls()
-	router.Run(":8080")
+	err := router.Run(":8080")
+	if err != nil {
+		return 
+	}
 }
--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,10 +1,13 @@
 package app
 
-import "github.com/burnout09/bookstore-users-api/controllers"
+import (
+	"github.com/burnout09/bookstore-users-api/controllers/ping"
+	"github.com/burnout09/bookstore-users-api/controllers/users"
+)
 
 func MapUrls() {
-	router.GET("/ping", controllers.Ping)
-	router.POST("/users/:user_id", controllers.GetUser)
-	router.POST("/users/search", controllers.SearchUser)
-	router.POST("/users", controllers.CreateUser)
+	router.GET("/ping", ping.Ping)
+	router.GET("/users/:user_id", users.GetUser)
+	router.GET("/users/search", users.SearchUser)
+	router.POST("/users", users.CreateUser)
 }
