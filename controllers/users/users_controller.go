562227d01b0599f507049201212e10091c2acd1b
Pool Malca <poolmalca09@gmail.com>
2021-06-16 01:42:22 -0500

    8. Cómo estructurar la persistencia de nuestro dominio

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/burnout09/bookstore-users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
@@ -41,7 +42,20 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("invalid user id, user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
 func SearchUser(c *gin.Context) {
