5d469cd8ea5640962e409587637dfd0b6db1f958
Pool Malca <pmmalca@indracompany.com>
2021-06-15 23:13:45 -0500

    7. Partiendo de nuestro dominio

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,18 +1,49 @@
 package users
 
 import (
+	"github.com/burnout09/bookstore-users-api/domain/users"
+	"github.com/burnout09/bookstore-users-api/services"
+	"github.com/burnout09/bookstore-users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented,"implement me!")
+	var user users.User
+
+	//bytes, err := ioutil.ReadAll(c.Request.Body)
+	//if err != nil {
+	//	//TODO: Handle errors
+	//	fmt.Println(err)
+	//	return
+	//}
+	//
+	//if err := json.Unmarshal(bytes, &user); err != nil {
+	//	//TODO: Handle json errors
+	//	fmt.Println(err)
+	//	return
+	//}
+
+	//ShouldBindJSON equivalente de usar ioutil.ReadAll y json.Unmarshal
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("Invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented,"implement me!")
+	c.String(http.StatusNotImplemented, "implement me!")
 }
 
 func SearchUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented,"implement me!")
+	c.String(http.StatusNotImplemented, "implement me!")
 }
--- services/user_service.go
+++ services/user_service.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"github.com/burnout09/bookstore-users-api/domain/users"
+	"github.com/burnout09/bookstore-users-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+}
