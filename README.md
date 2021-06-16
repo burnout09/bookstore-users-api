10d12ab3bfe297b0962e2863c0c91f2cb7c22639
Pool Malca <pmmalca@indracompany.com>
2021-06-16 12:59:05 -0500

    10. Configurar el cliente MySQL en Go

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bookstore-users-api
+
+## Creacion de MySQL Schema
+- CREATE SCHEMA `users_db` DEFAULT CHAR SET utf8 COLLATE utf8_spanish2_ci;
+
+## Variables de Entorno
+- mysql_users_username=root;mysql_users_password=burnout;mysql_users_host=127.0.0.1:3306;mysql_users_schema=users_db
--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"fmt"
+	"github.com/burnout09/bookstore-users-api/datasources/mysql/users_db"
 	"github.com/burnout09/bookstore-users-api/utils/date_utils"
 	"github.com/burnout09/bookstore-users-api/utils/errors"
 )
@@ -11,6 +12,10 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
+
 	result := usersBD[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
