d39f89e02adbdc769c4360246f04367c73b7e4fd
Pool Malca <pmmalca@indracompany.com>
2021-06-17 14:35:53 -0500

    14. Cómo actualizar filas

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -7,7 +7,9 @@ import (
 
 func MapUrls() {
 	router.GET("/ping", ping.Ping)
+
 	router.GET("/users/:user_id", users.GetUser)
-	router.GET("/users/search", users.SearchUser)
 	router.POST("/users", users.CreateUser)
+	router.PUT("/users/:user_id", users.UpdateUser)
+	router.PATCH("/users/:user_id", users.UpdateUser)
 }
--- services/user_service.go
+++ services/user_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"github.com/burnout09/bookstore-users-api/domain/users"
 	"github.com/burnout09/bookstore-users-api/utils/errors"
+	"strings"
 )
 
 func GetUser(userId int64) (*users.User, *errors.RestErr) {
@@ -23,4 +24,35 @@ func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	}
 
 	return &user, nil
+}
+
+func UpdateUser(isPartial bool, user users.User) (*users.User, *errors.RestErr) {
+	current, err := GetUser(user.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	if isPartial {
+		if len(strings.TrimSpace(user.FirstName)) > 0 {
+			current.FirstName = user.FirstName
+		}
+		if len(strings.TrimSpace(user.LastName)) > 0 {
+			current.LastName = user.LastName
+		}
+		if len(strings.TrimSpace(user.Email)) > 0 {
+			current.Email = user.Email
+		}
+	} else {
+		if err := user.Validate(); err != nil {
+			return nil, err
+		}
+		current.FirstName = user.FirstName
+		current.LastName = user.LastName
+		current.Email = user.Email
+	}
+
+	if err := current.Update(); err != nil {
+		return nil, err
+	}
+	return current, nil
 }
